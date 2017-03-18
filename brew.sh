6cfc86c69928cc2abd44366c337932d33a270d21
Elisha Terada <elishaterada@gmail.com>
2017-03-18 00:29:34 -0700

    List all brew packages to install

---
+++ brew.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Minimum Viable Tools (Short-Term)
+brew install git
+brew install n
+brew install yarn
+brew cask install sourcetree
+brew cask install visual-studio-code
+brew cask install phpstorm
+brew cask install google-chrome
+brew cask install firefox
+brew cask install go2shell
+brew cask install spectacle
+brew cask install lastpass
+brew cask install slack
+brew cask install clipy
+
+# Full Dev/Design Suites (Long-Term)
+# brew cask install vagrant
+# brew cask install virtualbox
+# brew cask install cyberduck
+# brew cask install mamp
+# brew cask install adobe-creative-cloud
+
+# Productivity Tools
+# brew cask install google-drive
+# brew cask install namechanger
+# brew cask install recordit
+# brew cask install skitch
+
+# Work Tools
+# brew cask install harvest
+# brew cask install joinme
+# brew cask install skype
+
+# Entertainment Apps
+# brew cask install sonos
+# brew cask install spotify
+# brew cask install quik
