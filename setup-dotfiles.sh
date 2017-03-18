0c3aecc6277a6517e98739c4475120079a699b97
Elisha Terada <elishaterada@gmail.com>
2017-03-18 01:39:37 -0700

    Simplify how dot files gets copied over

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -1,30 +1,10 @@
 #!/usr/bin/env bash
 
 # Copy dotfiles to home directories
-function doIt() {
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".macos" \
-    --exclude "brew-expansion.sh" \
-    --exclude "brew-work.sh" \
-    --exclude "brew.sh" \
-		--exclude "LICENSE" \
-    --exclude "MANUAL.md" \    
-    --exclude "node.sh" \    
-		--exclude "README.md" \
-		--exclude "setup-dotfiles.sh" \
-		--exclude "setup-tools.sh" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
-}
+cp -p .aliases ~/.aliases
+cp -p .bash_profile ~/.bash_profile
+cp -p .exports ~/.exports
+cp -p .gitignore ~/.gitignore
+cp -p .bashrc ~/.bashrc
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+source ~/.bash_profile
