a6b37e0b94abf72f09468bf7913f0ba146a4d5d4
Elisha Terada <>
2018-03-02 21:01:32 -0800

    Update expansions

--- brew-expansion.sh
+++ brew-expansion.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
 # Full Dev/Design Suites (Long-Term)
-brew install ansible
 brew install homebrew/php/wp-cli
 brew cask install vagrant
-brew cask install virtualbox
-brew cask install cyberduck
-brew cask install adobe-creative-cloud
 brew cask install ngrok
 brew cask install mamp
 
 # Productivity Tools
-brew cask install google-drive
+brew cask install google-backup-and-sync
 brew cask install namechanger
 
 # Entertainment Apps
